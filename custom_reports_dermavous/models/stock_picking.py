0d7ff80f9b50e5a455ebc8ef8cc18f48fc721e43
kholillutfi <muhammadkholillutfi@gmail.com>
2023-11-30 20:19:33 +0700

    update custom report dermavous

--- custom_reports_dermavous/models/stock_picking.py
+++ custom_reports_dermavous/models/stock_picking.py
@@ -1,19 +1,10 @@
 from odoo import fields, models
-from datetime import timedelta
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     po_eksternal_number = fields.Char('No. Po (Eksternal)') 
 
-    def calculate_date(self, day_num):
-        for rec in self.move_line_ids_without_package:
-            if rec.lot_id:
-                res = rec.lot_id.create_date + timedelta(days=day_num)
-        
-                return res
-            
-    
 
 
 
