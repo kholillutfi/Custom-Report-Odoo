a1139ff41f8bd2d92e923d50c3cbe9531eb774c5
kholillutfi <muhammadkholillutfi@gmail.com>
2023-12-09 15:20:36 +0700

    delete other module

--- custom_reports_dermavous/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import stock_picking
-from . import stock_move_line
-from . import account_move
--- custom_reports_dermavous/models/stock_move_line.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-from datetime import datetime
-
-class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
-
-    jml_box = fields.Integer(string='Jml Box')
-    note = fields.Char(string='Note')
-    netto = fields.Char('Netto')
